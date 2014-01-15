525e601bbfd3999065a18cea66942bee0d6ca18e
roopesh90 <roopesh90nair@live.com>
2014-01-15 21:46:58 +0530

    to print a second line to text, update3

--- yolofirst.asm
+++ yolofirst.asm
@@ -18,6 +18,12 @@ start:
 
 	text_string db 'Welcome to Yolo! An adventurous OS 1!!', 0
 
+	mov si, text_string2	; Put string position into SI
+	call print_string	; Call our string-printing routine
+
+	;jmp $			; Jump here - infinite loop!
+
+	text_string2 db 'Who are you?? Do ask yourself that!', 0
 
 print_string:			; Routine: output string in SI to screen
 	mov ah, 0Eh		; int 10h 'print char' function
@@ -32,13 +38,6 @@ print_string:			; Routine: output string in SI to screen
 .done:
 	ret
 
-	mov si, text_string2	; Put string position into SI
-	call print_string	; Call our string-printing routine
-
-	jmp $			; Jump here - infinite loop!
-
-	text_string2 db 'Who are you?? Do ask yourself that!', 0
-
-
+	
 	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
 	dw 0xAA55		; The standard PC boot signature
